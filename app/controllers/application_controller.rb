6bb48bc6099d45f527b9527c2ea66356221394db
nhattrung <trungtranqn91@gmail.com>
2017-02-28 07:54:49 +0700

    Sign up and Edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "app.controller.please_login"
+      redirect_to login_url
+    end 
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:warning] = t "app.controllers.not_found_user"
+      redirect_to root_url
+    end
+    redirect_to root_url unless current_user? @user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,49 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: :show
+
+  before_action :correct_user, only: [:edit, :update]
+  before_action :load_user, only: [:edit, :show, :update]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+
   def show
   end
 
-  def new
+  def new 
+    @user = User.new      
   end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      log_in @user
+      flash[:success] = t("view.user.hi") + "#{@user.name}, " + t("view.user.welcome")
+      redirect_to @user
+    else
+      render :new 
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params 
+      flash[:success] = t "app.controllers.update_success"
+      redirect_to @user
+    else      
+      render :edit 
+    end   
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation, :avatar
+  end
+
   def load_user
     @user = User.find_by id: params[:id]
     unless @user
-      flash[:warning] = t "app.controllers.not_find_user"
+      flash[:warning] = t "app.controllers.not_found_user"
       redirect_to root_path
     end  
   end
