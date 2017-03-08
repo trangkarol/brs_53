cbd7f22c09b7b474ee5035ba06b33f8e8316a7f7
nhattrung <trungtranqn91@gmail.com>
2017-03-08 09:12:57 +0700

    Show all book and search book by title, by author and by rating

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @categories = Category.all
+    @books = Book.list_newest_desc.paginate page: params[:page],
+      per_page: Settings.per_page_book
+    if params[:search]
+      @books = Book.search_all(params[:search]).list_newest_desc.
+        paginate page: params[:page], per_page: Settings.per_page_book
+    else params[:input_search]
+      case
+      when params[:find_by] == t("view.search.title")
+        @books = Book.search_title(params[:input_search]).list_newest_desc.
+          paginate page: params[:page], per_page: Settings.per_page_book
+      when params[:find_by] == t("view.search.author")
+        @books = Book.search_author(params[:input_search]).list_newest_desc.
+          paginate page: params[:page], per_page: Settings.per_page_book
+      else
+        @books = Book.search_rating(params[:input_search]).list_newest_desc.
+          paginate page: params[:page], per_page: Settings.per_page_book
+      end
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224145418) do
+ActiveRecord::Schema.define(version: 20170307125738) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170224145418) do
     t.string   "picture"
     t.decimal  "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float  "avg_rate"
     t.index ["category_id"], name: "index_books_on_category_id"
   end
 
